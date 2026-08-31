f6ae3bca3556f8d578c3e139d2b98876d01d7280
albetimohammed-pixel <albetimohammed@gmail.com>
2026-08-31 16:35:10 +0300

    Create Step3LessonsScreen for lesson addition

---
+++ lib/presentation/screens/create_plan/step3_lessons_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Step3LessonsScreen extends StatelessWidget {
+  const Step3LessonsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('إضافة الدروس')),
+      body: const Center(child: Text('خطوة إضافة الدروس')),
+    );
+  }
+}
+
